447084e11aa45accd0703d70acbcf4d006ec9a30
April Long <april11dance@gmail.com>
2019-09-11 23:09:37 -0600

    javascript underway

---
+++ javascript/game.js
@@ -0,0 +1,20 @@
+document.onkeyup=function(event)
+$(#randomNumber).on(click () {
+var randomize=Math.floor(Math.random()*50+50) //pick a number between 1-100
+
+$("#randomNumber").text(randomize); 
+
+var picture1= Math.floor(Math.random()*20=+1)
+var picture2= Math.floor(Math.random()*20=+1)
+var picture3= Math.floor(Math.random()*20=+1)
+var picture4= Math.floor(Math.random()*20=+1)
+
+var userTotal= 0;
+var wins=0;
+var losses=0; 
+
+
+
+
+
+}
